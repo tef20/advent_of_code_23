b2c55fca9fe86abae66c02ab4c22f8239c0be192
Chris Telford <68950813+tef20@users.noreply.github.com>
2023-12-02 00:17:37 -0800

    fixed tab alignment

--- day2_puzzle2.js
+++ day2_puzzle2.js
@@ -4,32 +4,32 @@
 //  🎄 press enter!
 
 const sumOfMinimumSetPowers = document
-	.querySelector('pre')
-	.textContent
-	.trim()
-	.split(/\n/)
-	.reduce((acc, cur, ind) => {
-  	const [, sample] = cur.split(": ");
-		const minimumSet = {};
+  .querySelector('pre')
+  .textContent
+  .trim()
+  .split(/\n/)
+  .reduce((acc, cur, ind) => {
+    const [, sample] = cur.split(": ");
+    const minimumSet = {};
     
     // matchAll returns an iterator that produces arrays containing: 
     //  - the match, followed by
     //  - the capture groups for the count and colour 
-		const draws = sample.matchAll(/(\d+) (\w+)/g);
+    const draws = sample.matchAll(/(\d+) (\w+)/g);
 		
-		let draw = draws.next();
+    let draw = draws.next();
 
-		while (!draw.done) {
-			const [, count, colour] = draw.value;
+    while (!draw.done) {
+      const [, count, colour] = draw.value;
       
-			minimumSet[colour] = Math.max(minimumSet[colour] ?? 0, +count);
+      minimumSet[colour] = Math.max(minimumSet[colour] ?? 0, +count);
 
-			draw = draws.next();
-		}
+      draw = draws.next();
+    }
 		
-		const power = Object.values(minimumSet).reduce((acc, cur) => acc * cur);
+    const power = Object.values(minimumSet).reduce((acc, cur) => acc * cur);
     
-		return acc + power;
-	}, 0);
+    return acc + power;
+  }, 0);
 
 console.log(`*** Result: ${sumOfMinimumSetPowers} ***`);
